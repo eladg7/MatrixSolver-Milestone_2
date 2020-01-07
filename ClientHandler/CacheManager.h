b12c4b1cd0a2d889e39bb448ae2ced5973aa33bc
elad <eladgarber@gmail.com>
2020-01-07 11:56:22 +0200

    creating initial interfaces

---
+++ ClientHandler/CacheManager.h
@@ -0,0 +1,12 @@
+//
+// Created by elad on 07/01/2020.
+//
+
+#ifndef MILESTONE_2_CACHEMANAGER_H
+#define MILESTONE_2_CACHEMANAGER_H
+
+class CacheManager {
+
+};
+
+#endif //MILESTONE_2_CACHEMANAGER_H
---
+++ ClientHandler/ClientHandler.h
@@ -0,0 +1,12 @@
+//
+// Created by elad on 07/01/2020.
+//
+
+#ifndef MILESTONE_2_CLIENTHANDLER_H
+#define MILESTONE_2_CLIENTHANDLER_H
+
+class ClientHandler {
+
+};
+
+#endif //MILESTONE_2_CLIENTHANDLER_H
