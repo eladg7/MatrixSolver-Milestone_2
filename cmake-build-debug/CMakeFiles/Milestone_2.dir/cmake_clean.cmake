908d8cde22442d35603a7c8b9091ae3c57f8af6d
elad <eladgarber@gmail.com>
2020-01-07 11:43:22 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Milestone_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Milestone_2.dir/main.cpp.o"
+  "Milestone_2"
+  "Milestone_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Milestone_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Milestone_2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Milestone_2.
+# This may be replaced when dependencies are built.
