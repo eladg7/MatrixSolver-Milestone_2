150c3b87fc42fd2df512b54970729664addbff73
yekaterina <yekaterina.batashov@live.biu.ac.il>
2020-01-09 18:22:11 +0200

    added client handler

--- ClientHandler/ClientHandler.h
+++ ClientHandler/ClientHandler.h
@@ -6,16 +6,24 @@
 #define MILESTONE_2_CLIENTHANDLER_H
 
 #include <iostream>
+#include <sys/socket.h>
 #include "Solver.h"
 #include "CacheManager.h"
 
 using namespace std;
 
-template<typename P,typename S>
+template<typename P, typename S>
 class ClientHandler {
 
+protected:
+    virtual int writeToClient(int clientFD, const char *message) {
+        send(clientFD , message , strlen(message) , 0 );
+        std::cout<<"Message sent\n"<<std::endl;
+        return 0;
+    };
+
 public:
-    virtual void handleClient(istream is,ostream os)=0;
+    virtual void handleClient(istream is, ostream os) = 0;
 
 };
 
